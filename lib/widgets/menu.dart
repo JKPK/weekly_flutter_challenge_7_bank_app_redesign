25e184730fb85717feb6c533aa48a09402ba1402
JKPK <jacek.kawiak@gmail.com>
2019-11-15 22:10:40 +0100

    Finished logo slide in animation.

--- lib/widgets/menu.dart
+++ lib/widgets/menu.dart
@@ -14,32 +14,47 @@ class Menu extends StatefulWidget {
   _MenuState createState() => _MenuState();
 }
 
-class _MenuState extends State<Menu> {
+class _MenuState extends State<Menu> with SingleTickerProviderStateMixin {
   bool isInit = false;
-  double fromTop;
-  Offset centre;
-  double radius;
-  double angle;
-  Offset logoPos;
+  double backgroundTopMargin;
+  Offset bibCircleCentre;
+  double bigCircleRadius;
+  double logoCircleAngle;
+  Offset logoPosition;
   double splashAnimation = 0;
+  bool showLogo = false;
+
+  AnimationController _logoAnimationController;
+  Animation<double> _logoAnimation;
+
   @override
   void didChangeDependencies() {
     if (!isInit) {
       isInit = true;
       updateAnimatedSlideIn();
+      updateLogoPosition();
     }
     super.didChangeDependencies();
   }
 
   void updateAnimatedSlideIn() {
-    fromTop = MediaQuery.of(context).size.height * 1.05 -
+    backgroundTopMargin = MediaQuery.of(context).size.height * 1.05 -
         MediaQuery.of(context).size.height * .657 * splashAnimation;
-    centre = Offset(MediaQuery.of(context).size.width * 0.5 - 30,
-        -MediaQuery.of(context).size.width * 1.2 - 330 + fromTop - 30);
-    radius = MediaQuery.of(context).size.width * 2;
-    angle = math.pi * .2 + math.pi * .3 * splashAnimation;
-    logoPos = Offset(centre.dx + radius * math.cos(angle),
-        centre.dy + radius * math.sin(angle));
+    bibCircleCentre = Offset(MediaQuery.of(context).size.width * 0.5,
+        -MediaQuery.of(context).size.width * 1.2 - 330 + backgroundTopMargin);
+    bigCircleRadius = MediaQuery.of(context).size.width * 2;
+  }
+
+  void updateLogoPosition() {
+    setState(() {
+      logoCircleAngle =
+          math.pi * .4 + math.pi * .1 * _logoAnimationController.value;
+      logoPosition = Offset(
+          bibCircleCentre.dx - 30 + bigCircleRadius * math.cos(logoCircleAngle),
+          bibCircleCentre.dy -
+              30 +
+              bigCircleRadius * math.sin(logoCircleAngle));
+    });
   }
 
   @override
@@ -49,8 +64,27 @@ class _MenuState extends State<Menu> {
         setState(() {
           splashAnimation = data;
           updateAnimatedSlideIn();
+          if (!showLogo && data > .9) {
+            showLogo = true;
+            _logoAnimationController.forward();
+          }
         });
       });
+
+    _logoAnimationController = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: 500,
+      ),
+    );
+
+    _logoAnimation = CurvedAnimation(
+      parent: _logoAnimationController,
+      curve: Curves.easeIn,
+    )..addListener(() {
+        updateLogoPosition();
+      });
+
     super.initState();
   }
 
@@ -96,15 +130,15 @@ class _MenuState extends State<Menu> {
         ),
         Positioned(
           child: ClipPath(
-            clipper: CircleCutClipper(fromTop),
+            clipper: CircleCutClipper(backgroundTopMargin),
             child: Container(
               color: Colors.white,
             ),
           ),
         ),
         Positioned(
-          top: logoPos.dy,
-          left: logoPos.dx,
+          top: logoPosition.dy,
+          left: logoPosition.dx,
           child: Container(
             height: 60,
             width: 60,
